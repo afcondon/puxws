aa525ac9e2f17735b6a6f3e5adee6850ae9b557d
Andrew Condon <andrew.condon@gmail.com>
2016-04-04 19:24:38 +0200

    change coding style for 2.x

--- src/Main.purs
+++ src/Main.purs
@@ -2,24 +2,24 @@ module Main where
 
 import Prelude hiding ((#), div)
 
-import Data.Either
-import Data.Foreign
-import Data.Foreign.Class
-import WebSocket
-import Control.Monad.Aff
+import Data.Either (Either(Right, Left))
+import Data.Foreign (F)
+import Data.Foreign.Class (class IsForeign, readJSON, readProp)
+import WebSocket (WEBSOCKET, Connection(Connection), Message(Message), URL(URL), runMessageEvent, runMessage, newWebSocket)
+import Control.Monad.Aff (Aff, later')
 import Network.HTTP.Affjax as A
 import Control.Apply ((*>))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Var (($=))
-import Control.Monad.Eff.Exception
+import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Console (CONSOLE(), log)
 
-import Pux (EffModel, start, renderToDOM)
+import Pux (EffModel, start, renderToDOM, noEffects)
 import Pux.Html (Html, (#), (!), div, p, button, text, span)
 import Pux.Html.Attributes (className)
-import Pux.Html.Events (onClick, onKeyUp)
-import Signal as S
+import Pux.Html.Events (onClick)
+import Signal (Signal) as S
 import Signal.Channel (CHANNEL(), Channel, channel, send, subscribe) as S
 
 -- |=================================    ACTIONS      =================================
@@ -34,7 +34,7 @@ data Action
 
 -- |=================================    STATE      =================================
 
-data State = State { counter :: Int, banner :: String, socket :: Connection }
+type State = { counter :: Int, banner :: String, socket :: Connection }
 
 initialState :: S.Channel Action -> String -> forall e. Eff (ws :: WEBSOCKET|e) State
 initialState chan url = do
@@ -43,7 +43,7 @@ initialState chan url = do
       let received = runMessage (runMessageEvent event)
       log "message received from websocket"
       S.send chan ((ReceiveWSData received) :: Action)
-  let state = State { counter: 0, banner: "initial string", socket: connection }
+  let state = { counter: 0, banner: "initial string", socket: connection }
   return state
 
 -- |=================================    Ajax      =================================
@@ -61,48 +61,44 @@ instance ajaxMessageIsForeign :: IsForeign AjaxMsg where
 -- |=================================    UPDATE      =================================
 update :: forall e. Action -> State -> EffModel State Action
             (ajax :: A.AJAX, console :: CONSOLE, ws :: WEBSOCKET | e)
-update action (State state) =
-  case action of
-    ButtonOne ->
-      { state: State state { counter = state.counter + 1 }
+update ButtonOne state =
+      { state: state { counter = state.counter + 1 }
       , effects: [ liftEff $ log "set view to ButtonOne" *> return Nop ] }
-    ButtonTwo ->
-      { state: State state { counter = state.counter - 1 }
+update ButtonTwo state =
+      { state: state { counter = state.counter - 1 }
       , effects: [ liftEff $ log "set view to ButtonTwo" *> return Nop ] }
-    ReceiveWSData msg ->
-      { state: State state { banner = msg }
-      , effects: []
-      }
-    ReceiveAJAXData msg ->
-      { state: State state { banner = msg }
+update (ReceiveWSData msg) state = noEffects $ state { banner = msg }
+update (ReceiveAJAXData msg) state =
+      { state: state { banner = msg }
       , effects: [ liftEff $ log ("Updated new state: " ++ msg) *> return Nop ]
       }
-    ButtonThree ->
-      { state: State state { banner = "Loading data from server..." }
+update ButtonThree state =
+      { state: state { banner = "Loading data from server..." }
       , effects: [ doAjaxCall ]
       }
-    ButtonFour ->
-      { state: State state
+update ButtonFour state =
+      { state: state
       , effects: [ doWebSocketCall state.socket ]
       }
-    Nop -> { state: State state, effects: [] }
-  where
-    -- don't know how to write signature for this function!
-    doAjaxCall = later' 1500 $ do
-      res <- A.get "http://localhost:8080/version"  -- requires something like json-server running on port 8080
-      let response = readJSON res.response :: F AjaxMsg
-      case response of
-        (Left err) -> do
-          liftEff $ log "Error parsing JSON!"
-          return Nop
-        (Right (AjaxMsg msg)) -> return $ ReceiveAJAXData msg.version
-    doWebSocketCall :: forall e. Connection -> Aff (ws :: WEBSOCKET | e) Action
-    doWebSocketCall (Connection ws) =
-      liftEff $ ws.send(Message "button four sends this message") *> return Nop
+update Nop state = noEffects $ state
+
+doAjaxCall :: forall e. Aff (console::CONSOLE,ajax::A.AJAX|e) Action
+doAjaxCall = later' 1500 $ do
+  res <- A.get "http://localhost:8080/version"  -- requires something like json-server running on port 8080
+  let response = readJSON res.response :: F AjaxMsg
+  case response of
+    (Left err) -> do
+      liftEff $ log "Error parsing JSON!"
+      return Nop
+    (Right (AjaxMsg msg)) -> return $ ReceiveAJAXData msg.version
+
+doWebSocketCall :: forall e. Connection -> Aff (ws :: WEBSOCKET | e) Action
+doWebSocketCall (Connection ws) =
+  liftEff $ ws.send(Message "button four sends this message") *> return Nop
 
 -- |=================================    VIEW      =================================
 view :: State -> Html Action
-view (State state) = div ! className "controls" # do
+view state = div ! className "controls" # do
   p # text (show state.counter)
   p # text (show state.banner)
   p ! className "btn-group" # do
@@ -121,7 +117,7 @@ main :: forall e. Eff ( ws::WEBSOCKET
                       , err::EXCEPTION
                       , console::CONSOLE | e ) Unit
 main = do
-  wsInput <- S.channel (ReceiveWSData "foo")
+  wsInput <- S.channel Nop
   appState <- initialState wsInput "ws://echo.websocket.org" -- forall e. Eff (ws :: WEBSOCKET|e) State
   let wsSignal = S.subscribe wsInput :: S.Signal Action
   app <- start
